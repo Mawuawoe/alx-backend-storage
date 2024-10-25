d06eb2ab704e98119318574667216f42ecc4a2c5
Mawuawoe <desmonddzakago@gmail.com>
2024-10-25 21:04:58 +0000

    task_1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- sql script to create a users table
+-- create a table
+CREATE TABLE IF NOT EXISTS users (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255),
+country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
