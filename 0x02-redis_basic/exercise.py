f744f8c67dbf6afdb63c2e535bd5bd7d0c1d1eee
Mawuawoe <desmonddzakago@gmail.com>
2024-11-01 20:55:17 +0000

    task_1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ implement a cache class, store data to redisRAM
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -14,11 +14,11 @@ class Cache:
 
     def __init__(self):
         """
-        connect to redis 
+        connect to redis
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         store data to redis,
@@ -26,4 +26,28 @@ class Cache:
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
-        return(key)
+        return (key)
+
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> Union[str, bytes, int, float, None]:
+        """
+        function to decode
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return (data)
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        method to return redis value as str
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        method to return redis value as int
+        """
+        return self.get(key, fn=int)
